81b6fb0f75c4cc819d393597cda185f8157d0e8a
Miguel Serrato <miguel@romerock.com>
2017-03-24 13:35:54 -0600

    last changes

--- FacebookLogin/FacebookLogin/HomeViewController.swift
+++ FacebookLogin/FacebookLogin/HomeViewController.swift
@@ -16,6 +16,9 @@ class HomeViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet var menuItem: UIBarButtonItem!
     @IBOutlet var contentView: UIView!
     @IBOutlet var loginButton: UIButton!
+    @IBOutlet var genderLabel: UILabel!
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var profilePictureImageView: FBSDKProfilePictureView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,9 +48,13 @@ class HomeViewController: UIViewController, UIGestureRecognizerDelegate {
         self.navigationItem.titleView = titleLabel
         
         if (FBSDKAccessToken.current() != nil) {
-            loginButton.isEnabled = false
+            loginButton.isHidden = true
+            contentView.isHidden = false
+            getInfo()
+        } else {
+            loginButton.isHidden = false
+            contentView.isHidden = true
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,26 +64,68 @@ class HomeViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBAction func loginButtonPressed(_ sender: Any) {
         let login:FBSDKLoginManager = FBSDKLoginManager()
-        login.logIn(withReadPermissions: ["public_profile", "email"], from: self) { result, error in
+        login.logIn(withReadPermissions: ["public_profile"], from: self) { result, error in
             if error != nil {
                 print("error \(error?.localizedDescription)")
             } else if (result?.isCancelled)! {
                 print("Cancelled")
             } else {
                 print("login with \(result)")
+                self.loginButton.isHidden = true
+                self.contentView.isHidden = false
+                self.getInfo()
             }
         }
     }
     
+    @IBAction func logOutButtonPressed(_ sender: Any) {
+        let logOut:FBSDKLoginManager = FBSDKLoginManager()
+        logOut.logOut()
+        loginButton.isHidden = false
+        contentView.isHidden = true
+    }
     
     func getInfo() {
-        let requestMe:FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me?fields=first_name, last_name, picture, email", parameters: nil, httpMethod: "GET")
+        profilePictureImageView.profileID = "me"
+        let requestMe:FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, first_name, last_name, gender, link"], httpMethod: "GET")
         let connection:FBSDKGraphRequestConnection = FBSDKGraphRequestConnection()
-        connection.add(requestMe) { connection, result, error in
-            if (result != nil) {
+        connection.add(requestMe) { (connection, result, error) -> Void in
+            if (error == nil) {
                 //if result
+                let firstName:String = (result! as AnyObject).object(forKey:"first_name") as! String
+                let lastName:String = (result! as AnyObject).object(forKey:"last_name") as! String
+                let fullName = firstName + " " + lastName
+                let gender:String = (result! as AnyObject).object(forKey:"gender") as! String
+                
+                let link:String = (result! as AnyObject).object(forKey:"link") as! String
+                
+                print(link)
+                
+                let name:String = (result! as AnyObject).object(forKey:"name") as! String
+                
+                print(name)
+                
+                self.nameAgeLabel.text = name
+                self.genderLabel.text = gender.capitalized
+                
             }
         }
+        connection.start()/*
+        FBSDKGraphRequest(graphPath: "me",
+                          
+                          parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email , gender, birthday"]).start(
+                            
+                            completionHandler: { (connection, result, error) -> Void in
+                                
+                                
+                                if (error == nil)
+                                    
+                                {
+                                    
+                                    print((result! as AnyObject).object(forKey:"email")!)
+                                    print((result! as AnyObject).object(forKey:"birthday")!)
+                                    print((result! as AnyObject).object(forKey:"gender")!)
+                                }})*/
     }
     
     /*
